8ac24327a1d848191e67babd5ea3b68e1e8c77cb
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-18 17:27:07 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hush::Application.config.secret_key_base = 'e59f0f7923b1b2a6a6ede9b5bbc918ec3be2ae2513a098f6c23853ab7b09ff8ab37f2b500e474262be57a395d4c10e6927c45594866623f0d1b55d01de3022ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hush::Application.config.session_store :cookie_store, key: '_hush_session'
