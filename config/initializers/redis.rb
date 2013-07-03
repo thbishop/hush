53f7be059c7bcfe9656736b94d7b20e186ad3e78
Tommy Bishop <bishop.thomas@gmail.com>
2013-07-02 21:27:14 -0700

    update how the redis config is handled

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,9 @@
-REDIS = Redis.new :host => ENV['HUSH_REDIS_HOST']
+uri = URI.parse ENV['HUSH_REDIS_URL']
+
+args = Hash.new.tap do |h|
+  %w(host password port).each do |item|
+    h[item.to_sym] = uri.send item.to_sym unless uri.send(item.to_sym).blank?
+  end
+end
+
+REDIS = Redis.new args
