fb6e39ae8f9c1cb34682c9ba80764b2befc46e59
Tommy Bishop <bishop.thomas@gmail.com>
2013-07-02 22:01:36 -0700

    update redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -6,6 +6,8 @@ if ENV['HUSH_REDIS_URL']
       h[item.to_sym] = uri.send item.to_sym unless uri.send(item.to_sym).blank?
     end
   end
+else
+  args = { :host => 'localhost', :port => '6379'}
 end
 
 REDIS = Redis.new args
